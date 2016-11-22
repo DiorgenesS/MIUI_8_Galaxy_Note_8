9b3ac16158fb317088d2a16e436281f8aeca87ee
Diorgenes <dyogenes_gd@hotmail.com>
2016-11-22 15:13:44 -0300

    Inital release

---
+++ phoneapps.mk
@@ -0,0 +1,35 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	AdvancedDisplay \
+	BasicDreams \
+	Bluetooth \
+	CertInstaller \
+	KeyChain \
+	LatinIME \
+	LiveWallpapers \
+	PacProcessor \
+	PicoTts \
+	PrintSpooler \
+	SamsungServiceMode \
+	Stk \
+	telresources \
+	WAPPushManager 
+
+private-phone-priv-apps := \
+	AudioFX \
+	BackupRestoreConfirmation \
+	CarrierConfig \
+	CellBroadcastReceiver \
+	CMAudioService \
+	CMSettingsProvider \
+	ExternalStorageProvider \
+	FusedLocation \
+	InputDevices \
+	OneTimeInitializer \
+	ProxyHandler \
+	SharedStorageBackup \
+	Shell \
+	StatementService 
+	 
---
+++ releasetools.py
@@ -0,0 +1,37 @@
+import common
+import edify_generator
+
+def RemoveDeviceAssert(info):
+  edify = info.script
+  for i in xrange(len(edify.script)):
+    if "ro.product" in edify.script[i]:
+      edify.script[i] = ''
+      return
+
+def AddAssertions(info):
+    edify = info.script
+    for i in xrange(len(edify.script)):
+        if " ||" in edify.script[i] and ("ro.product.device" in edify.script[i] or "ro.build.product" in edify.script[i]):
+            edify.script[i] = edify.script[i].replace(" ||", ' || getprop("ro.build.product") == "GT-N5100" || getprop("ro.product.device") == "kona3g" || getprop("ro.build.product") == "kona3g" ||')
+            return
+
+def AddArgsForFormatSystem(info):
+  edify = info.script
+  for i in xrange(len(edify.script)):
+    if "format(" in edify.script[i] and "/dev/block/platform/dw_mmc/by-name/SYSTEM" in edify.script[i]:
+      edify.script[i] = 'format("ext4", "EMMC", "/dev/block/platform/dw_mmc/by-name/SYSTEM", "0", "/system");'
+      return
+
+def WritePolicyConfig(info):
+  try:
+    file_contexts = info.input_zip.read("META/file_contexts")
+    common.ZipWriteStr(info.output_zip, "file_contexts", file_contexts)
+  except KeyError:
+    print "warning: file_context missing from target;"
+
+def FullOTA_InstallEnd(info):
+    WritePolicyConfig(info)
+    AddAssertions(info)
+
+def IncrementalOTA_InstallEnd(info):
+    AddAssertions(info)
