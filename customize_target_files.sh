5a2ebd009de78eced5d9fb3ea549221f14e88482
Diorgenes <dyogenes_gd@hotmail.com>
2016-11-24 11:12:43 -0300

    Added: device features

--- customize_target_files.sh
+++ customize_target_files.sh
@@ -10,3 +10,6 @@ cp -f other/file_contexts out/target_files/META/
 rm -rf out/target_files/SYSTEM/vendor/preinstall
 
 cp -f metadata/updater out/target_files/OTA/bin/
+
+#Added device features
+cp -f other/kona3g.xml out/target_files/SYSTEM/etc/device_features
